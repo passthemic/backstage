1ad2cf572926e4ce042d4996461f5ea6dd1272f8
James Stubblefield <jameswilliamiii@gmail.com>
2013-05-30 01:48:56 -0500

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PtmBackstage::Application.config.session_store :cookie_store, key: '_ptm_backstage_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PtmBackstage::Application.config.session_store :active_record_store
