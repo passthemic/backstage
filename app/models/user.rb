ed83bd4000c631bfcc72b95b29d0560edffc16cd
James Stubblefield <jameswilliamiii@gmail.com>
2013-05-30 02:15:01 -0500

    added Devise, added json failure for Devise,  and set up sessions and registrations

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+
+  before_save :ensure_authentication_token
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :token_authenticatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+
+  validates :username, :uniqueness => { :case_sensitive => false }
+
+end
