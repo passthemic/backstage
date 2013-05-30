886e7e9a47c74e8d1704483cf2bbd49278144640
James Stubblefield <jameswilliamiii@gmail.com>
2013-05-30 02:31:48 -0500

    updated the routes to add named routes for sign_in, sign_out, and sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ PtmBackstage::Application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      devise_for :users, controllers: {sessions: "sessions", registrations: "registrations"}
+      devise_for :users, controllers: {sessions: "sessions", registrations: "registrations"} do
+        post   'sessions'      => 'sessions#create',      :as => 'sign_in'
+        delete 'sessions'      => 'sessions#destroy',     :as => 'sign_out'
+        post   'registrations' => 'registrations#create', :as => 'sign_up'
+      end
       resources :verses
       resources :raps
     end
