052659583cbd442729344efe12e6756d28a1dd55
James Stubblefield <jameswilliamiii@gmail.com>
2013-05-30 02:25:14 -0500

    added namespaces for api/v1, updated sessions and registrations controllers to match this namespace.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < Devise::SessionsController
+class Api::V1::SessionsController < Devise::SessionsController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 PtmBackstage::Application.routes.draw do
 
-  devise_for :users, controllers: {sessions: "sessions", registrations: "registrations"}
-  resources :verses
-  resources :raps
+  namespace :api do
+    namespace :v1 do
+      devise_for :users, controllers: {sessions: "sessions", registrations: "registrations"}
+      resources :verses
+      resources :raps
+    end
+  end
 
 end
