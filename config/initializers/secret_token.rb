c7e2843e25f70a88dfd635928c992bf38f63ab17
James Stubblefield <jameswilliamiii@gmail.com>
2013-06-03 15:27:48 -0500

    updated secret token to ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PtmBackstage::Application.config.secret_token = 'e1a68ece14c8c88d7f2a6ff00297f658c98b0fceb484e889d4194affcefc4f92668bc4c5be9ffad7ef06921eed94a1306293f9c7a170821e09054824a9f7235a'
+PtmBackstage::Application.config.secret_token = ENV['SECRET_TOKEN']
