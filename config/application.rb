e6c9dd4a56ff7a26610cacf29681bf6fdd180d16
James Stubblefield <jameswilliamiii@gmail.com>
2013-06-03 16:03:18 -0500

    updated application to not initialize upon asset precompile on heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,10 @@ module PtmBackstage
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Used for heroku precompile
+    config.assets.initialize_on_precompile = false
+
+
   end
 end
